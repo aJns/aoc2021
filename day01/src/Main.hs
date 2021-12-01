ab253674006d08bbfe2960d68631cce6cb421381
Jonas Nikula <jonas.nikula@gmail.com>
2021-12-01 12:55:50 +0200

    Finish day 01.2

--- day01/src/Main.hs
+++ day01/src/Main.hs
@@ -5,7 +5,7 @@ main = do
     let filename = head args
     content <- readFile filename
     let cLines = lines content
-    let numberList = map (read::String->Int) cLines
+    let numberList = createSums [] $ map (read::String->Int) cLines
 
     let first = head numberList
     let count = checkDepth (tail numberList) first 0
@@ -18,3 +18,8 @@ checkDepth aList prev count = checkDepth (tail aList) next newCount
     where next = head aList
           newCount = if prev < next then count + 1
                                     else count
+
+createSums :: [Int] -> [Int] -> [Int]
+createSums sumList (a:b:c:xs) = createSums (sumList ++ [newSum]) ([b] ++ [c] ++ xs)
+    where newSum = a + b + c
+createSums sumList _ = sumList
