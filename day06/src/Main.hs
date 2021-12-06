623db3e7a8e4cbd2e4469b8c1ab74a59b6930b6f
aJns <nigula77@gmail.com>
2021-12-06 21:55:17 +0200

    Finish day06.2

--- day06/src/Main.hs
+++ day06/src/Main.hs
@@ -1,23 +1,48 @@
 import System.Environment
 import Data.Strings
+import Data.Map (Map, empty, insertWith, toList, elems)
+
+type FishMap = Map Int Int
+type FishPair = (Int, Int)
 
 main = do
     args <- getArgs
     let filename = head args
-    let days = read $ args !! 1
+    let days = (read::String->Int) $ args !! 1
     content <- readFile filename
     let cLines = lines content
     let fishes = map read $ strSplitAll "," $ head cLines
 
-    putStrLn $ show $ length $ modelFish days fishes
+    let initMap = fromInit fishes empty
+    let fishMap = modelFish days initMap
+    let result = foldl (+) 0 $ elems fishMap
+
+    putStrLn $ show $ result
 
 
+fromInit :: [Int] -> FishMap -> FishMap
+fromInit [] fishMap = fishMap
+fromInit fishes fishMap = fromInit b $ insertWith (+) a 1 fishMap
+    where a = head fishes
+          b = tail fishes
 
-nextDay :: Int -> [Int]
-nextDay 0 = [6, 8]
-nextDay a = [a-1]
+
+fromPairs :: [FishPair] -> FishMap -> FishMap
+fromPairs [] fishMap = fishMap
+fromPairs (x:xs) fishMap = fromPairs xs $ insertWith (+) (fst x) (snd x) fishMap
 
 
-modelFish :: Int -> [Int] -> [Int]
+nextDay :: FishMap -> FishMap
+nextDay fishMap = newMap
+    where fishList = concat $ map nextPair $ toList fishMap
+          newMap = fromPairs fishList empty
+
+
+nextPair :: FishPair -> [FishPair]
+nextPair (0, count) = [(6, count), (8, count)]
+nextPair (day, count) = [(day-1, count)]
+
+
+modelFish :: Int -> FishMap -> FishMap
 modelFish 0 dayZero = dayZero
-modelFish day dayZero = concat $ map nextDay $ modelFish (day -1) dayZero
+modelFish day dayZero = nextDay $ modelFish (day -1) dayZero
