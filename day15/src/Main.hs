ef8ec7ce39271acee7c14d72537e5404aa0b9afa
aJns <nigula77@gmail.com>
2021-12-16 20:42:21 +0200

    Asdf

--- day15/src/Main.hs
+++ day15/src/Main.hs
@@ -20,35 +20,37 @@ main = do
     let cLines = lines content
     let risks = map (map digitToInt) cLines
 
-    let incrementer = [0, 1] --, 2, 3, 4]
+    let incrementer = [0..4]
 
     let colRisks = map (incrementRow incrementer) risks
     let bigRisks = transpose $ map (incrementRow incrementer) $ transpose colRisks
 
-    let rows = length risks
-    let cols = length $ head risks
+    let rows = length bigRisks
+    let cols = length $ head bigRisks
     let keys = [[(i,j) | j <- [0..cols]] | i <- [0..rows]]
     let endNode = (rows-1, cols-1)
 
-    let riskMap = Map.fromList $ concat $ map (\(k,v) -> zip k v) $ zip keys risks
+    let riskMap = Map.fromList $ concat $ map (\(k,v) -> zip k v) $ zip keys bigRisks
     let graph = buildGraph Map.empty riskMap (Map.keys riskMap)
     let costMap = dijkstra graph ((0,0), endNode)
     let path = reconstructPath graph costMap endNode []
-    let pathCost = foldl (+) 0 $ map (\x -> riskMap ! x) $ drop 1 path
+    let pathCost = show $ foldl (+) 0 $ map (\x -> riskMap ! x) $ drop 1 path
 
-    putStrLn $ show $ bigRisks
+    let stringRepr = intercalate "\n" $ map (map (\x -> intToDigit x)) bigRisks
+
+    putStrLn $ pathCost
 
 
 
-incrementWithWrap :: Int -> Int
-incrementWithWrap a
-  | b > 9 = 1
-  | otherwise = b
-  where b = a + 1
+incrementWithWrap :: Int -> Int -> Int
+incrementWithWrap a b
+  | c > 9 = c - 9
+  | otherwise = c
+  where c = a + b
 
 
 incrementRow :: [Int] -> [Int] -> [Int]
-incrementRow cols row = concat $ map (\x -> map incrementWithWrap row) cols
+incrementRow cols row = concat $ map (\x -> map (incrementWithWrap x) row) cols
 
 
 reconstructPath :: Graph -> DistanceMap -> Node-> [Node] -> [Node]
